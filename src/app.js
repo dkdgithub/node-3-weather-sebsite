c8bc998ffc5b0b0057b076b26dd808db06e7dce1
Deepak Kumar <deepak.kumar@aeris.net>
2022-03-07 19:55:06 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -101,6 +104,6 @@ app.get("*", (req, res) => {
 });
 
 // app.listen() is used to start the server, first parameter is the port no and second parameter is the callback function.
-app.listen(3000, () => {
-  console.log(`Server is up on port 3000`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
